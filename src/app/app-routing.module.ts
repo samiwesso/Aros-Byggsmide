a6ff4d3c3b56ed9c524973b56270cafe77b4c699
Sami <sami-weso@hotmail.com>
2019-09-25 18:28:20 +0200

    navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'navigation', component: NavigationComponent },
 ];
 
 @NgModule({
